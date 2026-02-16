f6bb3c8acb4c170384669226922be99073dbe5c8
zs <15346602014@163.com>
2026-02-16 19:29:51 +0800

    feat:先清理记录

---
+++ myframework-cloud-framework/myframework-module-spring/src/main/java/com/kuma/boot/spring/web/swagger/SpringDocAutoConfiguration.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2020-2030, Shuigedeng (981376577@qq.com & https://blog.kumacloud.top/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kuma.boot.spring.web.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * SpringDoc OpenAPI 自动配置
+ *
+ * @author kuma
+ */
+@Configuration
+@ConditionalOnWebApplication
+@ConditionalOnClass(OpenAPI.class)
+@EnableConfigurationProperties(SpringDocProperties.class)
+public class SpringDocAutoConfiguration {
+
+    @Bean
+    public OpenAPI openAPI(SpringDocProperties properties) {
+        Info info = new Info()
+                .title(properties.getTitle())
+                .description(properties.getDescription())
+                .version(properties.getVersion())
+                .contact(new Contact()
+                        .name(properties.getContactName())
+                        .url(properties.getContactUrl()))
+                .license(new License()
+                        .name(properties.getLicenseName())
+                        .url(properties.getLicenseUrl()));
+
+        return new OpenAPI().info(info);
+    }
+}
---
+++ myframework-cloud-framework/myframework-module-spring/src/main/java/com/kuma/boot/spring/web/swagger/SpringDocProperties.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2020-2030, Shuigedeng (981376577@qq.com & https://blog.kumacloud.top/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kuma.boot.spring.web.swagger;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * SpringDoc 配置属性
+ *
+ * @author kuma
+ */
+@Data
+@ConfigurationProperties(prefix = "kuma.springdoc")
+public class SpringDocProperties {
+
+    /**
+     * API 文档标题
+     */
+    private String title = "API 文档";
+
+    /**
+     * API 文档描述
+     */
+    private String description = "RESTful API 接口文档";
+
+    /**
+     * API 版本
+     */
+    private String version = "1.0.0";
+
+    /**
+     * 联系人名称
+     */
+    private String contactName = "";
+
+    /**
+     * 联系人 URL
+     */
+    private String contactUrl = "";
+
+    /**
+     * 许可证名称
+     */
+    private String licenseName = "Apache 2.0";
+
+    /**
+     * 许可证 URL
+     */
+    private String licenseUrl = "https://www.apache.org/licenses/LICENSE-2.0.html";
+}
