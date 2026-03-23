de326d02980c1b88eb229d4378be42e38a17bdbc
zs <15346602014@163.com>
2026-03-23 21:11:45 +0800

    feat:office的授权与多余处理

--- kuma-boot-framework/kuma-boot-starter-office/src/main/java/com/kuma/boot/office/convert/config/StartAppRunner.java
+++ kuma-boot-framework/kuma-boot-starter-office/src/main/java/com/kuma/boot/office/convert/config/StartAppRunner.java
@@ -22,6 +22,9 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 /**
  * 服务初始化之后，执行方法
@@ -43,9 +46,10 @@ public class StartAppRunner implements CommandLineRunner {
 
     private void applyWordsLicense() {
         try {
-            InputStream is = new ClassPathResource("license.xml").getInputStream();
-            com.aspose.words.License license = new com.aspose.words.License();
-            license.setLicense(is);
+//            InputStream is = new ClassPathResource("license.xml").getInputStream();
+//            com.aspose.words.License license = new com.aspose.words.License();
+//            license.setLicense(is);
+            registerWord2412();
             LogUtils.info("aspose-words 授权成功");
         } catch (Exception e) {
             LogUtils.warn("aspose-words 授权失败（评估版水印保留）: {}", e.getMessage());
@@ -82,6 +86,42 @@ public class StartAppRunner implements CommandLineRunner {
             LogUtils.info("aspose-slides 授权成功");
         } catch (Exception e) {
             LogUtils.warn("aspose-slides 授权失败（评估版水印保留）: {}", e.getMessage());
+        }
+    }
+
+    public static void registerWord2412() {
+        try {
+            Class<?> zzodClass = Class.forName("com.aspose.words.zzod");
+            Constructor<?> constructors = zzodClass.getDeclaredConstructors()[0];
+            constructors.setAccessible(true);
+            Object instance = constructors.newInstance(null, null);
+            Field zzWws = zzodClass.getDeclaredField("zzWws");
+            zzWws.setAccessible(true);
+            zzWws.set(instance, 1);
+            Field zzVZC = zzodClass.getDeclaredField("zzVZC");
+            zzVZC.setAccessible(true);
+            zzVZC.set(instance, 1);
+
+            Class<?> zz83Class = Class.forName("com.aspose.words.zz83");
+            constructors.setAccessible(true);
+            constructors.newInstance(null, null);
+
+            Field zzZY4 = zz83Class.getDeclaredField("zzZY4");
+            zzZY4.setAccessible(true);
+            ArrayList<Object> zzwPValue = new ArrayList<>();
+            zzwPValue.add(instance);
+            zzZY4.set(null, zzwPValue);
+
+            Class<?> zzXuRClass = Class.forName("com.aspose.words.zzXuR");
+            Field zzWE8 = zzXuRClass.getDeclaredField("zzWE8");
+            zzWE8.setAccessible(true);
+            zzWE8.set(null, 128);
+            Field zzZKj = zzXuRClass.getDeclaredField("zzZKj");
+            zzZKj.setAccessible(true);
+            zzZKj.set(null, false);
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
