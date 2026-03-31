bda2855b62c9ca2d250ffcde7fc7d831763105c1
zs <15346602014@163.com>
2026-03-31 18:39:23 +0800

    fix:修复Prometheus

--- kuma-boot-framework/kuma-boot-starter-monitor/src/main/java/com/kuma/boot/monitor/alarm/plugin/email/wrapper/EmailWrapper.java
+++ kuma-boot-framework/kuma-boot-starter-monitor/src/main/java/com/kuma/boot/monitor/alarm/plugin/email/wrapper/EmailWrapper.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020-2030, Kuma (2569277704@qq.com & https://blog.kumacloud.top/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.kuma.boot.monitor.alarm.plugin.email.wrapper;
 
 import com.kuma.boot.monitor.alarm.core.loader.entity.RegisterInfo;
@@ -6,12 +22,12 @@ import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.HtmlEmail;
 
+/** 选择email报警时，必须指定对应的帐号信息 */
 public class EmailWrapper {
-   private EmailWrapper() {
-   }
 
    public static HtmlEmail genEmailClient() throws EmailException {
       RegisterInfo info = RegisterInfoLoaderHelper.load();
+
       HtmlEmail htmlEmail = new HtmlEmail();
       htmlEmail.setCharset("UTF-8");
       htmlEmail.setHostName(info.getEmailHost());
@@ -21,7 +37,6 @@ public class EmailWrapper {
       if (BooleanUtils.isTrue(info.getEmailSsl())) {
          htmlEmail.setSSLOnConnect(true);
       }
-
       return htmlEmail;
    }
 }
