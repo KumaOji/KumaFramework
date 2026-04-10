5458cc2bb86493fa94a06c886bcffae3cdb7d7d5
zs <15346602014@163.com>
2026-04-10 16:24:29 +0800

    fix(test): correct issues in KmcBootTestBase

--- kuma-boot-framework/kuma-boot-starter-test/src/main/java/com/kuma/boot/test/KmcBootTestBase.java
+++ kuma-boot-framework/kuma-boot-starter-test/src/main/java/com/kuma/boot/test/KmcBootTestBase.java
@@ -6,14 +6,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
-@AutoConfigureMockMvc
 public class KmcBootTestBase {
-   @Autowired
-   protected MockMvcTester mockMvcTester;
    @Autowired
    protected ConfigurableEnvironment configurableEnvironment;
 
@@ -23,7 +18,7 @@ public class KmcBootTestBase {
    @BeforeAll
    public static void initAll() {
       System.setProperty("kmc.running.in.test", "true");
-      LogUtils.info("-------------------\u5f00\u59cb\u6d4b\u8bd5-----------------", new Object[0]);
+      LogUtils.info("-------------------开始测试-----------------", new Object[0]);
    }
 
    @BeforeEach
@@ -36,7 +31,7 @@ public class KmcBootTestBase {
 
    @AfterAll
    public static void tearDownAll() {
-      LogUtils.info("-------------------\u6d4b\u8bd5\u7ed3\u675f-------------------", new Object[0]);
+      LogUtils.info("-------------------测试结束-------------------", new Object[0]);
    }
 
    @AfterEach
