defdde574ab2380bbeeea11ca6c374ed3a5a7429
zs <15346602014@163.com>
2026-04-13 11:10:58 +0800

    fix:fix modules dependence

--- kuma-boot-framework/kuma-boot-starter-data-jpa/src/main/java/com/kuma/boot/data/jpa/utils/BaseValidator.java
+++ kuma-boot-framework/kuma-boot-starter-data-jpa/src/main/java/com/kuma/boot/data/jpa/utils/BaseValidator.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020-2030, Shuigedeng (981376577@qq.com & https://blog.kumacloud.top/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.kuma.boot.data.jpa.utils;
 
 import jakarta.validation.ConstraintViolation;
@@ -10,24 +26,58 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import org.apache.commons.collections4.CollectionUtils;
 import org.hibernate.validator.HibernateValidator;
-import org.hibernate.validator.HibernateValidatorConfiguration;
 
+/**
+ * 基础校验器
+ *
+ */
 public final class BaseValidator {
-   private static final Validator validator = ((HibernateValidatorConfiguration)((HibernateValidatorConfiguration)Validation.byProvider(HibernateValidator.class).configure()).failFast(false)).buildValidatorFactory().getValidator();
+
+   private static final Validator validator;
+
+   static {
+      validator =
+              Validation.byProvider(HibernateValidator.class)
+                      .configure()
+                      .failFast(false)
+                      .buildValidatorFactory()
+                      .getValidator();
+   }
 
    private BaseValidator() {
+      super();
    }
 
+   /**
+    * 校验对象
+    *
+    * @param obj 对象
+    * @return 错误消息集合
+    */
    public static List<String> validate(Object obj) {
-      Set<ConstraintViolation<Object>> set = validator.validate(obj, new Class[0]);
-      return (List)((Set)Optional.ofNullable(set).orElse(Collections.emptySet())).stream().map(ConstraintViolation::getMessage).collect(Collectors.toList());
+      Set<ConstraintViolation<Object>> set = validator.validate(obj);
+      return Optional.ofNullable(set).orElse(Collections.emptySet()).stream()
+              .map(ConstraintViolation::getMessage)
+              .collect(Collectors.toList());
    }
 
+   /**
+    * 对象是否有效
+    *
+    * @param obj 对象
+    * @return 是否有效
+    */
    public static boolean isValid(Object obj) {
-      Set<ConstraintViolation<Object>> set = validator.validate(obj, new Class[0]);
+      Set<ConstraintViolation<Object>> set = validator.validate(obj);
       return CollectionUtils.isEmpty(set);
    }
 
+   /**
+    * 对象是否无效
+    *
+    * @param obj 对象
+    * @return 是否无效
+    */
    public static boolean isInvalid(Object obj) {
       return !isValid(obj);
    }
--- kuma-boot-framework/kuma-boot-starter-data-mongodb/src/main/java/com/kuma/boot/data/mongodb/autoconfigure/MongodbAutoConfiguration.java
+++ kuma-boot-framework/kuma-boot-starter-data-mongodb/src/main/java/com/kuma/boot/data/mongodb/autoconfigure/MongodbAutoConfiguration.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright (c) 2020-2030, Shuigedeng (981376577@qq.com & https://blog.kumacloud.top/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.kuma.boot.data.mongodb.autoconfigure;
 
+import com.kuma.boot.common.constant.StarterNameConstants;
 import com.kuma.boot.common.utils.log.LogUtils;
-import com.kuma.boot.data.mongodb.autoconfigure.properties.MongodbProperties;
 import com.kuma.boot.data.mongodb.converter.DBObjectToJsonNodeConverter;
 import com.kuma.boot.data.mongodb.converter.JsonNodeToDocumentConverter;
 import com.kuma.boot.data.mongodb.converter.LocalDateTimeToString;
@@ -11,6 +27,7 @@ import com.kuma.boot.data.mongodb.converter.StringToLocalDateTime;
 import com.kuma.boot.data.mongodb.helper.config.MongoStartedEventListener;
 import com.kuma.boot.data.mongodb.helper.utils.ImportExportUtil;
 import com.kuma.boot.data.mongodb.helper.utils.MongoHelper;
+import com.kuma.boot.data.mongodb.autoconfigure.properties.MongodbProperties;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.InitializingBean;
@@ -30,25 +47,27 @@ import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
+/**
+ * es配置类
+ *
+ * @author shuigedeng
+ * @version 2022.03
+ * @since 2020/5/3 06:47
+ */
 @AutoConfiguration
 @EnableConfigurationProperties({MongodbProperties.class})
-@ConditionalOnProperty(
-   prefix = "kuma.boot.data.mongodb",
-   name = {"enabled"},
-   havingValue = "true"
-)
+@ConditionalOnProperty(prefix = MongodbProperties.PREFIX, name = "enabled", havingValue = "true")
 public class MongodbAutoConfiguration implements InitializingBean {
-   public MongodbAutoConfiguration() {
-   }
 
+   @Override
    public void afterPropertiesSet() throws Exception {
-      LogUtils.started(MongodbAutoConfiguration.class, "kuma-boot-starter-data-mongodb", new String[0]);
+      LogUtils.started(MongodbAutoConfiguration.class, StarterNameConstants.DATA_MONGODB_STARTER);
    }
 
    @Primary
    @Bean
    public MongoCustomConversions customConversions() {
-      List<Converter<?, ?>> converters = new ArrayList(2);
+      List<Converter<?, ?>> converters = new ArrayList<>(2);
       converters.add(DBObjectToJsonNodeConverter.INSTANCE);
       converters.add(JsonNodeToDocumentConverter.INSTANCE);
       converters.add(new LocalDateTimeToString());
@@ -58,21 +77,36 @@ public class MongodbAutoConfiguration implements InitializingBean {
       return new MongoCustomConversions(converters);
    }
 
+   // @Bean
+   // @ConditionalOnBean(MongoTemplate.class)
+   // public BaseMongoDAO baseMongoDAO(MongoTemplate mongoTemplate) {
+   //	return new MongoDaoSupport(mongoTemplate);
+   // }
+
+   /**
+    * MongodbHelperAutoConfiguration
+    *
+    * @author shuigedeng
+    * @version 2026.03
+    * @since 2025-12-19 09:30:45
+    */
    @Configuration
    public static class MongodbHelperAutoConfiguration {
+
       @Autowired
       private MongoDatabaseFactory mongoDatabaseFactory;
+
       @Autowired
       private MongoMappingContext mongoMappingContext;
 
-      public MongodbHelperAutoConfiguration() {
-      }
-
       @Bean
       public MappingMongoConverter mappingMongoConverter() {
-         DbRefResolver dbRefResolver = new DefaultDbRefResolver(this.mongoDatabaseFactory);
-         MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, this.mongoMappingContext);
-         converter.setTypeMapper(new DefaultMongoTypeMapper((String)null));
+         DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+         MappingMongoConverter converter =
+                 new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+
+         // 此处是去除插入数据库的 _class 字段
+         converter.setTypeMapper(new DefaultMongoTypeMapper(null));
          return converter;
       }
 
