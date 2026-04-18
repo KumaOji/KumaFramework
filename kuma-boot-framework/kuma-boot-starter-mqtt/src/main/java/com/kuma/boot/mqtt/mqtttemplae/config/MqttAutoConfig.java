36ef02778a494b50fa0f9e63386e09fddcd75151
zs <15346602014@163.com>
2026-04-18 13:25:00 +0800

    fix(project4): fix full startup

--- kuma-boot-framework/kuma-boot-starter-mqtt/src/main/java/com/kuma/boot/mqtt/mqtttemplae/config/MqttAutoConfig.java
+++ kuma-boot-framework/kuma-boot-starter-mqtt/src/main/java/com/kuma/boot/mqtt/mqtttemplae/config/MqttAutoConfig.java
@@ -2,10 +2,12 @@ package com.kuma.boot.mqtt.mqtttemplae.config;
 
 import com.kuma.boot.mqtt.mqtttemplae.template.MqttTemplate;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
+@ConditionalOnBean(SpringMqttBrokerProperties.class)
 @ConditionalOnProperty(
    prefix = "spring.mqtt-broker",
    name = {"enabled"},
