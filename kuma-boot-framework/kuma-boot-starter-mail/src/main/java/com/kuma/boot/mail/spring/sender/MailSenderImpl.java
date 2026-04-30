f90ed7867018f2f47a2f5d5292568ef73128ae49
zs <15346602014@163.com>
2026-04-30 09:55:35 +0800

    feat(mail): add mail content

--- kuma-boot-framework/kuma-boot-starter-mail/src/main/java/com/kuma/boot/mail/spring/sender/MailSenderImpl.java
+++ kuma-boot-framework/kuma-boot-starter-mail/src/main/java/com/kuma/boot/mail/spring/sender/MailSenderImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020-2030, Kuma (2569277704@qq.com & https://blog.kumacloud.top/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.kuma.boot.mail.spring.sender;
 
 import com.kuma.boot.common.utils.log.LogUtils;
@@ -5,70 +21,89 @@ import com.kuma.boot.mail.spring.event.MailSendEvent;
 import com.kuma.boot.mail.spring.model.MailDetails;
 import com.kuma.boot.mail.spring.model.MailSendInfo;
 import jakarta.mail.MessagingException;
-import java.io.File;
-import java.time.LocalDateTime;
 import org.springframework.boot.mail.autoconfigure.MailProperties;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.util.StringUtils;
 
-public class MailSenderImpl implements MailSender {
+import java.io.File;
+import java.time.LocalDateTime;
+
+public class MailSenderImpl implements com.kuma.boot.mail.spring.sender.MailSender {
+
    private final JavaMailSender mailSender;
    private final ApplicationEventPublisher eventPublisher;
    private final MailProperties mailProperties;
 
-   public MailSenderImpl(JavaMailSender mailSender, ApplicationEventPublisher eventPublisher, MailProperties mailProperties) {
+   /** 配置文件中我的qq邮箱 */
+   // @Value("${spring.mail.properties.from}")
+   // private String defaultFrom;
+   public MailSenderImpl(
+           JavaMailSender mailSender, ApplicationEventPublisher eventPublisher, MailProperties mailProperties) {
       this.mailSender = mailSender;
       this.eventPublisher = eventPublisher;
       this.mailProperties = mailProperties;
    }
 
+   /**
+    * 发送邮件
+    *
+    * @param mailDetails 邮件参数
+    * @return boolean 发送是否成功
+    */
+   @Override
    public MailSendInfo sendMail(MailDetails mailDetails) {
       MailSendInfo mailSendInfo = new MailSendInfo(mailDetails);
       mailSendInfo.setSentDate(LocalDateTime.now());
 
       try {
-         this.checkMail(mailDetails);
-         this.sendMimeMail(mailDetails);
+         // 1.检测邮件
+         checkMail(mailDetails);
+         // 2.发送邮件
+         sendMimeMail(mailDetails);
          mailSendInfo.setSuccess(true);
       } catch (Exception e) {
          mailSendInfo.setSuccess(false);
          mailSendInfo.setErrorMsg(e.getMessage());
-         LogUtils.error("\u53d1\u9001\u90ae\u4ef6\u5931\u8d25: [{}]", new Object[]{mailDetails, e});
+         LogUtils.error("发送邮件失败: [{}]", mailDetails, e);
       } finally {
-         this.eventPublisher.publishEvent(new MailSendEvent(mailSendInfo));
+         // 发布邮件发送事件
+         eventPublisher.publishEvent(new MailSendEvent(mailSendInfo));
       }
-
       return mailSendInfo;
    }
 
+   /**
+    * 构建复杂邮件信息类
+    *
+    * @param mailDetails 邮件发送设置
+    */
    private void sendMimeMail(MailDetails mailDetails) throws MessagingException {
-      MimeMessageHelper messageHelper = new MimeMessageHelper(this.mailSender.createMimeMessage(), true);
-      String from = StringUtils.hasText(mailDetails.getFrom()) ? mailDetails.getFrom() : this.mailProperties.getUsername();
+      // true表示支持复杂类型
+      MimeMessageHelper messageHelper = new MimeMessageHelper(mailSender.createMimeMessage(), true);
+      String from = StringUtils.hasText(mailDetails.getFrom()) ? mailDetails.getFrom() : mailProperties.getUsername();
       messageHelper.setFrom(from);
       messageHelper.setSubject(mailDetails.getSubject());
       if (mailDetails.getTo() != null && mailDetails.getTo().length > 0) {
          messageHelper.setTo(mailDetails.getTo());
       }
-
       if (mailDetails.getCc() != null && mailDetails.getCc().length > 0) {
          messageHelper.setCc(mailDetails.getCc());
       }
-
       if (mailDetails.getBcc() != null && mailDetails.getBcc().length > 0) {
          messageHelper.setBcc(mailDetails.getBcc());
       }
-
+      // 是否展示html
       boolean showHtml = mailDetails.getShowHtml() != null && mailDetails.getShowHtml();
       messageHelper.setText(mailDetails.getContent(), showHtml);
       if (mailDetails.getFiles() != null) {
-         for(File file : mailDetails.getFiles()) {
+         for (File file : mailDetails.getFiles()) {
             messageHelper.addAttachment(file.getName(), file);
          }
       }
 
-      this.mailSender.send(messageHelper.getMimeMessage());
-      LogUtils.info("\u53d1\u9001\u90ae\u4ef6\u6210\u529f\uff1a[{}]", new Object[]{mailDetails});
+      mailSender.send(messageHelper.getMimeMessage());
+      LogUtils.info("发送邮件成功：[{}]", mailDetails);
    }
 }
